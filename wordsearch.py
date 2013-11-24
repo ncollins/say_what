14ef4cba9f7eb517dc97c6c8eb0919c60ba48f6d
Polina Giralt <polina.giralt@gmail.com>
2013-11-24 17:45:47 -0500

    fixes bug where result were being sent for legislator even when not matched, since it was still returning results outside of the "if" statement

--- wordsearch.py
+++ wordsearch.py
@@ -10,8 +10,8 @@ from login import keys
 
 sunlight.config.API_KEY = keys['sunlight']
 
-phrase = 'the'  # input_phrase
-start_date = '2011-10-05'  # input_date - 183
+phrase = 'jobs'  # input_phrase
+start_date = '2012-10-05'  # input_date - 183
 end_date = '2013-11-16'  # input_date + 183
 legislator_id = 'K000352'  # input_bioguide_id
 
@@ -19,7 +19,10 @@ legislator_id = 'K000352'  # input_bioguide_id
 # this phrase in the congressional record during a period of time.
 
 
-def count_phrase_for_legislator(phrase, legislator_id, start_date, end_date):
+def count_phrase_for_legislator(phrase,
+                                legislator_id,
+                                start_date,
+                                end_date):
 
     for cw_record in capitolwords.phrases_by_entity(
         "legislator",   # We're getting all legislators
@@ -27,7 +30,8 @@ def count_phrase_for_legislator(phrase, legislator_id, start_date, end_date):
         phrase=phrase,  # this word
         start_date=start_date,
         end_date=end_date,
-        sort="count",   # sorted by how much they say
+        # sort="count",   # sorted by how much they say
+        sort="legislator",   # sorted by who said it
     )[:]:
 
         legislator = congress.legislators(
@@ -38,15 +42,19 @@ def count_phrase_for_legislator(phrase, legislator_id, start_date, end_date):
 
         if len(legislator) >= 1:  # If we were able to find the legislator
             legislator = legislator[0]  # (this is a search, so it's a list)
-            if cw_record['legislator'] == legislator_id:
-                # print("%s. %s said %s %s times" % (
-                #     legislator['title'],
-                #     legislator['last_name'],
-                #     phrase,
-                #     int(cw_record['count']))
-                #     )
-                return (legislator['title'] + ' ' +legislator['last_name'], int(cw_record['count']))
-        return (legislator['title'] + ' ' +legislator['last_name'], int(cw_record['count']))
+            # if cw_record['legislator'] == legislator_id:
+            print("{0}. {1} said {2} {3} times".format(
+                    legislator['title'],
+                    legislator['last_name'],
+                    phrase,
+                    int(cw_record['count']))
+                    )
+                # return (legislator['title'] + ' ' +
+                #         legislator['last_name'],
+                #      int(cw_record['count']))
+        # return (legislator['title'] +
+        #    ' ' +legislator['last_name'],
+        #      int(cw_record['count']))
     return ('', 0)
 
 print(count_phrase_for_legislator(phrase, legislator_id, start_date, end_date))
